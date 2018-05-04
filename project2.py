8123567e7ca4f2ddda2d5578d023db7a26ba2425
CHobbes007 <30755959+CHobbes007@users.noreply.github.com>
2018-05-03 23:15:58 -0500

    Major Changes to Working State.  Accepts single command from priveledge exec.

--- project2.py
+++ project2.py
@@ -7,6 +7,8 @@ print('Version:                 | 0.1')
 print('Python Version:          | 3.x')
 print('Tab:                     | 4 spaces')
 print('Library Dependencies:    | netmiko; csv; getpass')
+print('Special Thanks-')
+print('Tim Grabrian')
 print('')
 # End Header
 
@@ -18,18 +20,26 @@ README:
 Expected CSV Headers/Columns
 HOSTNAME,IP_ADDRESS,IOS_TYPE
 
+This project assumes a Windows OS to make relative folder paths for saving
+output.  
 
 '''
 
 # Import Library Dependencies
 import getpass
 import csv
+import os
 
 from netmiko import ConnectHandler
+from paramiko.ssh_exception import SSHException
+from netmiko.ssh_exception import NetMikoTimeoutException
+from netmiko.ssh_exception import NetMikoAuthenticationException
 
-
+#######################################
 # Define Global Variables
-# Input Username
+# Input Username, Password and Enable Password
+# Input CSV
+#######################################
 
 username = str(input("Please Enter your username: "))
 print('The username you entered was: ', username)
@@ -42,57 +52,175 @@ except Exception as error:
 else:
     print('Password has been accepted.')
 
-# Manipulate variables to add an apostrophe
-username="'"+username+"'"
-p="'"+p+"'"
+# Input Enable password without echoing passwor
+try:
+    ep = getpass.getpass(prompt='Please enter your Enable password: ')
+except Exception as error:
+    print('ERROR: No Enable password entered', error)
+else:
+    print('Enable Password has been accepted.')
 
 # Input CSV File Name
+print ('')
+print ('')
+print('#######################################')
+print('Example Local Path: ')
+print('inventory.csv')
+print('Example Windows Relative Path: ')
+print('CSV_Input\inventory.csv')
+print('CSV Headers:')
+print('Node_Name,IP_Address,Device_Type')
+print('#######################################')
 file_name=str(input("Please Enter the input CSV file name: "))
 print('Input CSV file:', file_name)
+print ('')
+print ('')
 # enddef
 
 
-# Open CSV file
-def r_cvs_file():
-    with open(file_name) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        hostnames = []
-        ip_addrs = []
-        devices = []
+#######################################
+# Define Functions
+#######################################
+
+
+
+
+# Define connection
+def my_connection(hostname, device, ip_addr, username, p, ep):
+    """
+    Attempts connection to device with NetMiko and handles errors.
+    Args:
+        device      string - Device type (per NetMiko)
+        ip_addr     string - Device IP address
+        username    string - Device username
+        p           string - Device password
+        ep          string - Device enable password
+
+    Returns:
+        NetMiko connection handle if successfully connected,
+        False if connection fails.
+    """
+
+    try:
+        connection = ConnectHandler(device_type=device, ip=ip_addr,
+                                    username=username,
+                                    password=p, secret=ep)
+    except NetMikoTimeoutException:
+        print(hostname + " Connection timed out!")
+        return False
+    except NetMikoAuthenticationException:
+        print(hostname + " Authentication failed!")
+        return False
+    except NetMikoAuthenticationException:
+        print(hostname + " Authentication failed!")
+        return False
+    except TimeoutError:
+        print(hostname + " Other timeout error!")
+        return False
+    # endtry
+
+    return connection
+# enddef
+
+
+# Define single command
+def run_cmd(command, prompt, connection, quiet=False, show_prompt=True,
+            delay_factor=1):
+    """
+    Runs a single command on a device and displays output.
+    Args:
+        command         string - Command to execute
+        prompt          string - Device prompt (typically hostname#)
+        connection      object - Connection to device (NetMiko)
+        quiet           bool - Hide command output? Defaults to False
+        show_prompt     bool - Echo the prompt/command? Defaults to true
+        delay_factor    int - Change timeout of command-run. Defaults to 1.
+    Returns:
+        Output of command
+    """
+
+    try:
+        output = connection.send_command(command, delay_factor=delay_factor)
+    except IOError:
+        # Seen when delay not long enough
+        print_err("send_command(" + command + ") IOError!")
+        return  # Skip additional output.
+    # endtry
+
+    if show_prompt and not quiet:
+        print(prompt + " " + command)  # Echo prompt and executed command
+    # endif
+
+    if not quiet:
+        print(output)  # Echo output of command executed
+    # endif
+
+    return output  # Return output of executed command
+# enddef
+
+# def run_cmds():
+#
+# def run_en_cmd():
+#
+# def run_en_cmds():
+#
+# def run_pair_cmds():
+
+
+
+# Open CSV file with DictReader
+def read_csv_file(command):
+    with open(file_name, mode='r') as csvfile:
+        readCSV = csv.DictReader(csvfile)
         for row in readCSV:
-            hostname = [row[0]]
-            ip_addr = [row[1]]
-            device = [row[2]]
+            hostname = row['Node_Name']
+            ip_addr = row['IP_Address']
+            device = row['Device_Type']
 
-            # Change from list to string
-            hostname = str(hostname)
-            ip_addr = str(ip_addr)
-            device = str(device)
+            # Uncomment below for testing
+            # print(hostname)
+            # print(ip_addr)
+            # print(device)
+            # print(username)
+            # print(p)
+            # print(ep)
 
-            # Manipulate string to remove brackets
-            hostname = hostname.replace('[','').replace(']','')
-            ip_addr = ip_addr.replace('[','').replace(']','')
-            device = device.replace('[','').replace(']','')
+            node = {
+                'device_type': device,
+                'ip': ip_addr,
+                'username': username,
+                'password': p,
+                'secret': ep,
+                'verbose': False,
+            }
 
-            # Testing
-            print(hostname)
-            print(ip_addr)
-            print(device)
-            print(username)
-            print(p)
+            # Print for readability
+            print('\n#######################################\n')
+            print(">>>>>>>>> {0}".format(row['Node_Name']))
+            print(">>>>>>>>> {0}\n".format(row['IP_Address']))
+            # Uncomment below for testing
+            # print(hostname)
+            # print(ip_addr)
+            # print(device)
+            # print(username)
+            # print(p)
+            # print(ep)
+            # print(command)
+            net_connect = my_connection(hostname, device, ip_addr, username,
+            p, ep)
+            if not net_connect:
+                continue  # Skip this node due to connection issues.
+            prompt = net_connect.find_prompt()
+            run_cmd(command, prompt, net_connect)
+            print("\n>>>>>>>>> End <<<<<<<<<\n\n")
 
-            # Connect
-            net_connect = ConnectHandler(device_type=device, ip=ip_addr, username=username, password=p)
-            net_connect.find_prompt()
-            output = net_connect.send_command("show run | in hostname")
-            print(output)
-# enddef
 
 
+
+# Main Appilication
 def main():
-    print ('') 
-    print ('')
-    r_cvs_file()
+    command = 'show ip int br'
+    read_csv_file(command)
     exit()
 
 if __name__ == "__main__":
